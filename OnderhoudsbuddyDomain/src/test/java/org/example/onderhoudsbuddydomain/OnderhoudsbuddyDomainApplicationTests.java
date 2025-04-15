a6bca5a0f24685084bedc877bdfdb21c3fbc78a6
Usher Missiedjan <531498@student.fontys.nl>
2025-04-15 15:01:48 +0200

    removed circular dependency

--- OnderhoudsbuddyDomain/src/test/java/org/example/onderhoudsbuddydomain/OnderhoudsbuddyDomainApplicationTests.java
+++ OnderhoudsbuddyDomain/src/test/java/org/example/onderhoudsbuddydomain/OnderhoudsbuddyDomainApplicationTests.java
@@ -3,7 +3,6 @@ package org.example.onderhoudsbuddydomain;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = OnderhoudsbuddyWebApplication.class)
 class OnderhoudsbuddyDomainApplicationTests {
 
     @Test
